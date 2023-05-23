ae628b4a8e17d96a36927d39c5e09945e615f683
Jondav84 <121706134+Jondav84@users.noreply.github.com>
2023-05-23 12:24:04 -0700

    I spoke too soon

--- movies.js
+++ movies.js
@@ -1,6 +1,7 @@
 /** @format */
 
 $(document).ready(function () {
+  /// standard update local storage function for the list items
   function updateLocalStorage() {
     localStorage.setItem(
       "movies",
@@ -9,17 +10,13 @@ $(document).ready(function () {
       )
     );
   }
-
-  $("#rating")
-    .on("input", function () {
-      $("span").text(
-        parseFloat($(this).val()) === 10
-          ? parseFloat($(this).val()).toFixed(0)
-          : parseFloat($(this).val()).toFixed(1)
-      );
-    })
-    .trigger("input");
-
+  //standard retrive local storage items for the ul
+  if (localStorage.getItem("movies")) {
+    JSON.parse(localStorage.getItem("movies")).forEach((movie) => {
+      addListItem();
+    });
+  }
+  //function for to generate li's
   function addListItem() {
     $("ul").append(
       $("<li>")
@@ -35,9 +32,8 @@ $(document).ready(function () {
         )
     );
   }
-
+  //function to create list item on button click
   $("#post").on("click", function (e) {
-    e.preventDefault();
     if ($("#title").val().length < 2) {
       alert("Title must Contain at least 2 letters");
       return;
@@ -47,10 +43,14 @@ $(document).ready(function () {
     $("#ratingValue").text(0.0);
     updateLocalStorage();
   });
-
-  if (localStorage.getItem("movies")) {
-    JSON.parse(localStorage.getItem("movies")).forEach((movie) => {
-      addListItem();
-    });
-  }
+  //function to display range value and control its movement
+  $("#rating")
+    .on("input", function () {
+      $("span").text(
+        parseFloat($(this).val()) === 10
+          ? parseFloat($(this).val()).toFixed(0)
+          : parseFloat($(this).val()).toFixed(1)
+      );
+    })
+    .trigger("input");
 });
